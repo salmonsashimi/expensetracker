7372538f8abc2636631aecc6b75c4fce1763bae2
ME <ME>
2021-10-15 00:25:27 +0800

    setup prod webpack build

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => (
     <header>
-        <h1>Expensify</h1>
+        <h1>expensetracker</h1>
         <NavLink to='/' activeClassName='is-active' exact>Dashboard</NavLink>
         <NavLink to='/create' activeClassName='is-active'>Create expense</NavLink>
         <NavLink to='/help' activeClassName='is-active'>Help</NavLink>
